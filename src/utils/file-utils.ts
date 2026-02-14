5cdd3953ca5ea53f24219ea19fc9afff986cfc1e
ishayoyo <ishay.almuly@gmail.com>
2026-02-14 19:15:40 +0200

    Update version to 1.0.2 in package.json and remove unused fsSync import in file-utils.ts

--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs/promises';
-import * as fsSync from 'fs';
 import * as path from 'path';
 import * as csv from 'csv-parse/sync';
 import * as csvStringify from 'csv-stringify/sync';
